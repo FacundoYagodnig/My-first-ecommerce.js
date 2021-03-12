e79db7a0a92181a42c292899fc2f5621e0822e30
Facundo Yagodnig <facundoyagodnig@gmail.com>
2021-03-12 15:18:49 -0300

    deployment 2

--- script/index.js
+++ script/index.js
@@ -16,7 +16,7 @@ let localStore = localStorage.getItem("users");
 
 const fetchData = async () => {
   try {
-    const res = await fetch("/public/api.json");
+    const res = await fetch("/api.json");
     const data = await res.json();
     recorrerLista(data);
   } catch (error) {
